12e8cf1e17caa93482c51f561cd574a4d76e4ee7
Aya Ait <aya.aitoulahyane@gmail.com>
2022-12-24 13:53:36 +0100

    [Add] Log in & Page Admin interface

--- src/App/Main.java
+++ src/App/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("presentation/views/loginpage.fxml"));
-        primaryStage.setTitle("Easy Project Management tool");
+        primaryStage.setTitle("Java Projet : Plannification des projets");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
